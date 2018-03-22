ab584f499597b3eecdcc161a7e72ec8c6768c662
nekrotzar <peixeocavalovoador@hotmail.com>
2018-03-22 14:37:44 +0000

    Define number of threads when running tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,11 +1,38 @@
 #!/bin/bash
+PROGRAM=$1
+NUM_THREADS=${2:-4}
 
-echo "Solve all sudoku puzzles."
 
-for filename in input/*.in
-do
-	echo "=========================================="
-	echo "Puzzle: $filename "
-	./sudoku-serial "$filename"
-	echo "=========================================="
-done
+if [[ -n "$PROGRAM" ]]; then
+	#Set number of threads
+	
+	printf "\n*******************************************\n"
+	printf "\tSolve all sudoku puzzles.\n\n"
+
+	numfiles=(input/*.in)
+	numfiles=${#numfiles[@]}
+	echo "$numfiles PUZZLES" 
+
+	if [[ "$PROGRAM" == "sudoku-serial" ]]; then
+		echo "MODE: serial"
+	elif [[ "$PROGRAM" == "sudoku-omp" ]]; then
+		export OMP_NUM_THREADS="$NUM_THREADS"
+
+		echo "MODE: omp parallel"
+		echo "NUMBER OF THREADS: $NUM_THREADS"
+	fi
+	printf "*******************************************\n\n"
+
+	COUNT=1
+	for filename in input/*.in
+	do
+		echo "TEST: $COUNT"
+		echo "FILE: $filename "
+		COUNT=$((COUNT+1))
+		./"$PROGRAM" "$filename"
+		echo "=========================================="
+	done
+else
+	echo "Error passing arguments: must pass program name as argument, e.g sudoku-serial ."
+fi
+
